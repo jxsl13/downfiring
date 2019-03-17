4d9f7d6d5b377e50c17000060426d8c93df6460d
John Behm <jxsl13@googlemail.com>
2019-03-17 16:36:43 +0100

    refactoring: less code to show file name of chosen file

--- main/static/main/js/upload.js
+++ main/static/main/js/upload.js
@@ -1,14 +1,6 @@
-function fileName(str)
-{
-   var base = str.split('/');
-   base = base[base.length - 1];
-   base = base.split('\\');
-   base = base[base.length - 1];
-   return base;
-}
-
-
 $('#ufr-file').on('change',function(){
-    var fileName = $(this).val();
-    $('#ufr-file-label').text(fileName(fileName))
+	// splits the fake file path and returns the file name
+    var fileName = $(this).val().split(/[\\/]/).pop();
+
+    $('#ufr-file-label').text(fileName);
 })
